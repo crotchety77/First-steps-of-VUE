250c2c8ba6db7dce1b15817f45f19012becaefff
Andrey Vlasov <98467872+crotchety77@users.noreply.github.com>
2025-01-03 09:49:58 +0300

    First-Step-Todo-Delete

--- app.js
+++ app.js
@@ -2,7 +2,24 @@ const App = {
     data(){
         return {
             counter: 0, //должны быть видны в шаблоне
-            titleCounter: 'Счетчик:'
+            titleCounter: 'Счетчик:',
+            title: 'Список заметок',
+            placeholderVue: 'Введите название заметки',
+            inputValue: "",
+            notes: ['Заметка 1', 'Заметка 2']
+        }
+    },
+    methods:{
+        inputChangeHandler(event){
+            // console.log('inputChangeHandler', event.target.value);
+            this.inputValue = event.target.value; // перерисовыем конкретно это место
+        },
+        addNewNote(event) {
+            this.notes.push(this.inputValue);
+            this.inputValue = "";
+        },
+        delNote(index){
+            this.notes.splice(index, 1); // Удаляем заметку по индексу
         }
     }
 }
